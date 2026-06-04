6e9be2ac75b7ce20f09380de27aa844e1d5a70a5
Sangchual Cha <sangchual.cha@hotmail.com>
2026-06-04 15:37:12 -0400

    implement RealTimeActiveBuyerWindow with sliding 10-minute window

--- src/main/java/practice/interview/coupang/RealTimeActiveBuyerWindow.java
+++ src/main/java/practice/interview/coupang/RealTimeActiveBuyerWindow.java
@@ -1,5 +1,12 @@
 package practice.interview.coupang;
 
+import java.security.SecureRandom;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 /**
  * Real-Time Active Buyer Window
  *
@@ -17,4 +24,44 @@ package practice.interview.coupang;
  * 	•	Optimize for very large log volume
  */
 public class RealTimeActiveBuyerWindow {
+
+    record Log(Integer userId, LocalDateTime timestamp) {};
+    PriorityQueue<Log> window = new PriorityQueue<>(Comparator.comparing(Log::timestamp));
+    Duration windowDuration = Duration.ofMinutes(10);
+
+    public void addLog(Integer userId, LocalDateTime timestamp) {
+        window.add(new Log(userId, timestamp));
+    }
+
+    private void refreshWindow(LocalDateTime asOfCurrent) {
+        while(window.peek() != null &&  window.peek().timestamp.plus(windowDuration).isBefore(asOfCurrent)) {
+            window.poll();
+        }
+    }
+
+    public Set<Integer> findLast10MinActiveUser(LocalDateTime end) {
+        refreshWindow(end);
+        return findLast10MinActiveUser(new ArrayList<>(window), end);
+    }
+
+    private Set<Integer> findLast10MinActiveUser(List<Log> logs, LocalDateTime end) {
+        logs.sort(Comparator.comparing(Log::timestamp));
+        LocalDateTime start = end.minusMinutes(10);
+        LocalDateTime curr = logs.get(0).timestamp;
+        List<Log> within10Min = logs.stream().filter(log -> (log.timestamp.isAfter(start) || log.timestamp.isEqual(start)) && (log.timestamp.isEqual(end) || log.timestamp.isBefore(end))).toList();
+        return within10Min.stream().map(log -> log.userId).collect(Collectors.toSet());
+    }
+
+    public static void main(String[] args) {
+        RealTimeActiveBuyerWindow buyerWindow = new RealTimeActiveBuyerWindow();
+
+        LocalDateTime base = LocalDateTime.of(2026, 2, 13, 0, 0, 0);
+        SecureRandom random = new SecureRandom();
+
+        IntStream.range(0, 10000).forEach(i-> {
+            buyerWindow.addLog(random.nextInt(10), base.minusSeconds(random.nextInt(3) * i));
+        });
+
+        buyerWindow.findLast10MinActiveUser(base.minusMinutes(2));
+    }
 }
